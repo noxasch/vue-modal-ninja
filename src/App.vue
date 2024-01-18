2fd55a90fc5b80a90604d3ef07f782c614a1ba34
Alexander Dischberg <noxasch@gmail.com>
2024-01-18 23:52:33 +0800

    feat: using props and emits

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,59 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref, onMounted } from 'vue';
+  import Modal from './components/Modal.vue';
+
+  // reactive state
+  // const count = ref(0)
+
+  const title = 'My First Vue App!';
+  const header = 'Sign up for the giveaway!';
+  const text = 'Grab you ninja swag for half price!';
+  const showModal = ref(false);
+
+  function toggleModal() {
+    showModal.value = !showModal.value;
+  }
+  // name.value is null until the component is mounted
+  // const name = ref(null);
+
+  // function handleClick() {
+  //   console.log(name.value);
+  //   name.value.classList.add('active');
+  // }
+
+  // onMounted(() => {
+  //   name.value.focus();
+  // });
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <!-- <input type="text" ref="name">
+  <button @click="handleClick">Click Me</button> -->
+  <Modal v-if="showModal"
+    v-bind:header="header"
+    :text="text"
+    theme="sale"
+    @toggle-modal="toggleModal"
+  />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <button @click="toggleModal">Click Me</button>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  --webkit-font-smoothing: antialiased;
+  --moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
